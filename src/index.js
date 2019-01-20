b80684b5e6db30ad861e70d7c614bccbb4b0dca5
onlymg <me@onlymg.com>
2019-01-20 13:18:55 +0100

    wrappd everything in a react component

--- src/index.js
+++ src/index.js
@@ -1,60 +1,161 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
 const three = window.THREE = require('three')
+const OrbitControls = require('three-orbit-controls')(three)
+
 require('three/examples/js/loaders/OBJLoader')
 require('three/src/loaders/TextureLoader')
 
-const OrbitControls = require('three-orbit-controls')(three)
+class Renderer extends Component {
 
-const container = document.getElementById('root')
+  componentDidMount() {
 
-const camera = new three.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
-camera.position.set(10, 5, 5)
+    const width = window.innerWidth, 
+          height = window.innerHeight * 0.9
 
-const scene = new three.Scene()
+    // init camera
+    this.camera = new three.PerspectiveCamera(45, width / height, 1, 1000)
+    this.camera.position.set(0, 0, 20)
 
-const pointLight = new three.PointLight(0xffffff, 0.8)
-const ambientLight = new three.AmbientLight(0xcccccc, 0.4)
-camera.add(pointLight);
-camera.add(ambientLight)
+    // add the scene to render in
+    this.scene = new three.Scene()
+    
+    // add light to scene
+    this.camera.add(new three.PointLight(0xffffff, 0.8))
+    this.camera.add(new three.AmbientLight(0xcccccc, 0.4))
+    
+    // add camera to scene
+    this.scene.add(this.camera)
 
-scene.add(camera)
+    // init renderer
+    this.renderer = new three.WebGLRenderer()
+    this.renderer.setClearColor(0xf0f0f0, 1.0)
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+    this.renderer.setSize(width, height)
 
-// load texture
-const texture = new three.TextureLoader().load('assets/textures/grid.jpg')
+    // append canvas
+    this.container.appendChild(this.renderer.domElement)
 
-// render two cubes
-const material = new three.MeshBasicMaterial({ map: texture })
-const cube1 = new three.Mesh(new three.BoxGeometry(5, 5, 5), material)
-const cube2 = new three.Mesh(new three.BoxGeometry(2, 2, 2), material)
-cube2.position.z = 3.5
+    // controls
+    const controls = new OrbitControls(this.camera)
+    controls.enableDamping = true
+    controls.dampingFactor = 0.95
+    controls.enableZoom = true
 
-scene.add(cube1)
-scene.add(cube2)
+    // add objects from props
+    this.props.objects.forEach(element => this.scene.add(element))
 
-// render boy
-new three.OBJLoader().load('assets/models/boy.obj', boy => {
-  boy.traverse(child => child.isMesh ? child.material.map = texture : null)
-  boy.position.x = -5
-  scene.add(boy)
-});
+    // render
+    this.start()
+  }
 
-const renderer = new three.WebGLRenderer()
-renderer.setClearColor(0xf0f0f0, 1.0)
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(window.innerWidth, window.innerHeight)
-container.appendChild(renderer.domElement)
 
-const controls = new OrbitControls(camera)
-controls.enableDamping = false
-controls.dampingFactor = 0.95
-controls.enableZoom = true
+  componentWillReceiveProps(nextProps) {
+    console.log(1)
+    if(this.props.objects !== nextProps.objects) {
+      this.props.objects.forEach(element => this.scene.remove(element))
+      nextProps.objects.forEach(element => this.scene.add(element))
+    }
+  }
 
-function render() {
-  renderer.render(scene, camera)
+  shouldComponentUpdate() {
+    return false
+  }
+
+  start() {
+    // start the browser animation
+    if (!this.frameID) {
+      this.frameID = requestAnimationFrame(this.animate.bind(this))
+    }
+  }
+
+  stop() {
+    // cancels the browser animation
+    cancelAnimationFrame(this.frameID)
+  }
+
+  animate() {
+    this.frameID = requestAnimationFrame(this.animate.bind(this))
+    this.renderScene()
+  }
+
+  renderScene() {
+    this.renderer.render(this.scene, this.camera)
+  }
+
+  componentWillUnmount() {
+    this.stop()
+    this.container.removeChild(this.renderer.domElement)
+  }
+
+  render() {
+    return <div ref={elem => this.container = elem} />
+  }
 }
 
-function animate() {
-  requestAnimationFrame(animate)
-  render()
+class Model extends Component {
+  render() {
+    return null
+  }
 }
 
-animate()
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      objectOffset: 0,
+      objects: []
+    }
+  }
+
+  componentDidMount() {
+
+    // example cubes
+    const texture = new three.TextureLoader().load('assets/textures/grid.jpg')
+    const material = new three.MeshPhongMaterial({ map: texture })
+    const cube1 = new three.Mesh(new three.BoxGeometry(5, 5, 5), material)
+    const cube2 = new three.Mesh(new three.BoxGeometry(2, 2, 2), material)
+    cube2.position.z = 3.5
+    this.objectOffset = 2
+
+    this.setState({ ...this.state, objects: [ cube1, cube2 ] })
+  }
+
+  addCube() {
+
+    // adds cube with random color and width to scene
+
+    const x = getRandomInteger(1, 5)
+    const material = new three.MeshPhongMaterial({ color: getRandomColor() })
+    const cube = new three.Mesh(new three.BoxGeometry(x, x, x), material)
+
+    cube.position.z = 5 * this.objectOffset
+    this.objectOffset++
+
+    this.setState({ ...this.state, objects: [ ...this.state.objects, cube ] })
+  }
+
+  render() {
+    return (
+      <div>
+        <Renderer objects={this.state.objects} />
+        <div style={{ padding: 20 }}>
+          <button onClick={this.addCube.bind(this)}>Add Cube</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomColor() {
+  return Math.floor(Math.random() * 16777215)
+}
