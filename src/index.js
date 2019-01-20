781a7651c7932b2db1e07ad561ef48764e5dbcb3
onlymg <me@onlymg.com>
2019-01-20 14:31:31 +0100

    chore and added shadows and a plane

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 const three = window.THREE = require('three')
-const OrbitControls = require('three-orbit-controls')(three)
 
 require('three/examples/js/loaders/OBJLoader')
+require('three/examples/js/controls/OrbitControls')
 require('three/src/loaders/TextureLoader')
 
 class Renderer extends Component {
@@ -22,9 +22,21 @@ class Renderer extends Component {
     this.scene = new three.Scene()
     
     // add light to scene
-    this.camera.add(new three.PointLight(0xffffff, 0.8))
-    this.camera.add(new three.AmbientLight(0xcccccc, 0.4))
-    
+    const light = new three.DirectionalLight(0xffffff, 0.9)
+    light.castShadow = true
+    light.position.set(20, 20, 0)
+
+    this.camera.add(light)
+    this.camera.add(new three.AmbientLight(0xffffff, 0.9))
+
+    // add a plane 
+    const planeGeometry = new THREE.PlaneBufferGeometry(60, 60, 22, 22)
+    const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff })
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+    plane.receiveShadow = true
+    plane.position.z = -2.5
+    this.scene.add(plane)
+
     // add camera to scene
     this.scene.add(this.camera)
 
@@ -33,14 +45,16 @@ class Renderer extends Component {
     this.renderer.setClearColor(0xf0f0f0, 1.0)
     this.renderer.setPixelRatio(window.devicePixelRatio)
     this.renderer.setSize(width, height)
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = three.PCFSoftShadowMap;
 
     // append canvas
     this.container.appendChild(this.renderer.domElement)
 
     // controls
-    const controls = new OrbitControls(this.camera)
+    const controls = new three.OrbitControls(this.camera)
     controls.enableDamping = true
-    controls.dampingFactor = 0.95
+    controls.dampingFactor = 0.99
     controls.enableZoom = true
 
     // add objects from props
@@ -52,7 +66,6 @@ class Renderer extends Component {
 
 
   componentWillReceiveProps(nextProps) {
-    console.log(1)
     if(this.props.objects !== nextProps.objects) {
       this.props.objects.forEach(element => this.scene.remove(element))
       nextProps.objects.forEach(element => this.scene.add(element))
@@ -94,18 +107,12 @@ class Renderer extends Component {
   }
 }
 
-class Model extends Component {
-  render() {
-    return null
-  }
-}
-
 class App extends Component {
 
   constructor() {
     super()
+    this.objectOffset = 0
     this.state = {
-      objectOffset: 0,
       objects: []
     }
   }
@@ -114,11 +121,19 @@ class App extends Component {
 
     // example cubes
     const texture = new three.TextureLoader().load('assets/textures/grid.jpg')
-    const material = new three.MeshPhongMaterial({ map: texture })
-    const cube1 = new three.Mesh(new three.BoxGeometry(5, 5, 5), material)
-    const cube2 = new three.Mesh(new three.BoxGeometry(2, 2, 2), material)
+    const material = new three.MeshStandardMaterial({ map: texture })
+    const cube1 = new three.Mesh(new three.BoxBufferGeometry(5, 5, 5), material)
+    const cube2 = new three.Mesh(new three.BoxBufferGeometry(2, 2, 2), material)
+    
+    cube1.castShadow = true; 
+    cube1.receiveShadow = true; 
+    cube2.castShadow = true; 
+    cube2.receiveShadow = true; 
+
     cube2.position.z = 3.5
-    this.objectOffset = 2
+    
+    this.objectOffset++
+    this.objectOffset++
 
     this.setState({ ...this.state, objects: [ cube1, cube2 ] })
   }
@@ -128,11 +143,12 @@ class App extends Component {
     // adds cube with random color and width to scene
 
     const x = getRandomInteger(1, 5)
-    const material = new three.MeshPhongMaterial({ color: getRandomColor() })
+    const material = new three.MeshStandardMaterial({ color: getRandomColor() })
     const cube = new three.Mesh(new three.BoxGeometry(x, x, x), material)
+    cube.castShadow = true
+    cube.receiveShadow = true
 
-    cube.position.z = 5 * this.objectOffset
-    this.objectOffset++
+    cube.position.z = 5 * this.objectOffset++
 
     this.setState({ ...this.state, objects: [ ...this.state.objects, cube ] })
   }
